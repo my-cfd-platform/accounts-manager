2fc83b4c78bf45f0f5502d2010cd75f63411c8c0
amigin <amigin@gmail.com>
2023-12-20 23:16:29 +0200

    Fixes with GrpcUrl

--- src/settings.rs
+++ src/settings.rs
@@ -16,7 +16,7 @@ pub struct SettingsModel {
     pub my_sb_tcp_host_port: String,
     pub default_account_balance: f64,
     pub default_account_trading_group: String,
-    pub accounts_persistence_grpc_url: String,
+    pub accounts_manager_persistence_grpc_url: String,
     pub accounts_default_currency: Option<String>,
     pub my_telemetry: String,
     pub seq_conn_string: String,
@@ -42,7 +42,7 @@ impl service_sdk::my_grpc_extensions::GrpcClientSettings for SettingsReader {
     async fn get_grpc_url(&self, name: &'static str) -> String {
         if name == crate::grpc_client::AccountsManagerPersistenceGrpcClient::get_service_name() {
             let read_access = self.settings.read().await;
-            return read_access.accounts_persistence_grpc_url.clone();
+            return read_access.accounts_manager_persistence_grpc_url.clone();
         }
 
         panic!("Unknown grpc service name: {}", name)
